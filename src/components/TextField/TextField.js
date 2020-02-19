49fe1e61a8eeb7d042f3f4648d55d0bd859d25df
Julian Garcia <julian.programmer.developer@gmail.com>
2020-02-18 22:55:38 -0600

    Add event handlers for increase of items to TextField component

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import './TextField.css'
+import ListItem from '../ListItem/ListItem';
 
 export default class TextField extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {clickCount:0};
+        this.state = {clickCount:0,items:[], new_item:''};
         this.onClick = this.onClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-    onClick(){
-        this.setState(state => ({
-            clickCount: this.state.clickCount + 1
+    onClick(event){
+        this.setState(previousState => ({
+            clickCount: previousState.clickCount + 1,
+            items: [...previousState.items, previousState.new_item],
         }));
-        console.log("It was clicked" + this.state.clickCount)
+
+        console.log("It was clicked" + this.state.new_item)
     }
+    handleChange({target}){
+        this.setState(previousState => ({
+            [target.name]: target.value
+        }));
+
+        console.log("It was clicked" + target);
+    }
+
     render(){
         return(
             <div className="TextField-form">
+                <ListItem items={this.state.items}/>
                 <form action="">
                     <div className="textfield">
-                        <input type="text" placeholder="Type text here"/>
+                        <input type="text" name='new_item' value={this.state.new_item} onChange={this.handleChange} placeholder="Type text here"/>
                     </div>
                     <div className="button">
                         <input type="button" onClick={this.onClick}value="Add"/>
