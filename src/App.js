96c7d0c62c88d2ad61e24deeb22c56414ee5ea12
Julian Garcia <julian.programmer.developer@gmail.com>
2020-02-18 22:50:43 -0600

    Remove ListItem from App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import './App.css';
 import Title from './components/Title/Title';
 import TextField from './components/TextField/TextField';
-import ListItem from './components/ListItem/ListItem';
 export default class App extends React.Component {
   render(){
   return (
     <div>
       <div className="App">
         <Title />
-        <ListItem />
         <TextField />
-        
-        <h1>App</h1>
       </div>
     </div>
   );
