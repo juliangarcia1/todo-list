cc7f97a13ced45229db64a4239c84bb36ca6072f
Julian Garcia <jggsoriano@box.com>
2020-03-06 19:20:20 -0600

    Add reducers files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from "redux";
-import{ itemReducer } from "./itemReducer";
+import itemReducer from "./itemReducer";
 
 export default combineReducers({
-    items: itemReducer
+    data: itemReducer
 });
--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,12 +1,21 @@
-import { ADD_ITEM, DELETE_ITEM } from "../actions/types"
+import { ADD_ITEM, DELETE_ITEM, SET_SOURCE } from "../actions/types";
+import {SOURCE_TYPE_LOCAL, JSON_KEY_LOCAL} from '../constants';
 
+const jsonKey = 'items.json';
+const json = window.localStorage.getItem(jsonKey) || JSON.stringify([], null, 2)
+const newJson = JSON.parse(json)
+const intialState = {items: newJson,
+                     jsonSource: SOURCE_TYPE_LOCAL,
+                     jsonKey: JSON_KEY_LOCAL};
 
-const itemReducer = (state, action) => {
+const itemReducer = (state = intialState, action) => {
     switch (action.type) {
         case ADD_ITEM:
-            return [...state, action.payload];
+            return {items: [...state.items, action.payload]};
         case DELETE_ITEM:
-            return state.filter(item=>item.id !== action.payload)
+            return {items: state.items.filter(item=>item.key !== action.payload)};
+        case SET_SOURCE:
+            return {...state, jsonSource: action.payload};
         default:
             return state;
     }
